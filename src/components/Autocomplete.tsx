3ffcba54dc374ff0f3bada9523721d25ebaec0c0
Nabil Kharrich <nabilkharrich4@gmail.com>
2025-03-23 21:22:29 +0100

    feat: add input markup and styles

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,5 +1,28 @@
+import styles from "./../styles/Autocomplete.module.scss";
+
 function Autocomplete() {
-    return <div>Autocomplete</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <span className={styles.title}>Countries explorer</span>
+                <span className={styles.description}>
+                    Look for a country to see if it's in the list
+                </span>
+            </div>
+            <div className={styles.inputWrapper}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Italy"
+                />
+                {<span className={styles.icon}>/</span>}
+            </div>
+            <div className={styles.bottom}>
+                <span className={styles.description}>Tech assessment</span>
+                <span className={styles.description}>Nabil Kharrich</span>
+            </div>
+        </div>
+    );
 }
 
 export default Autocomplete;
